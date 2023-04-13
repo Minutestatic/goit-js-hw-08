51bea4b7bc17be8dddbeac8db7df32e93b9c829a
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-04-14 02:39:25 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log('galleryItems');
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const cardGallery = createsGalleryPictures(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardGallery);
+
+function createsGalleryPictures(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`;
+    })
+    .join('');
+}
+
+const simpleLightbox = new SimpleLightbox('.gallery__link', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
