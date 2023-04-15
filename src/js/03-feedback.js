6570bcc73b5a6da6e2f327d484929d8739f253ee
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-04-15 21:34:03 +0300

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onSaveInput, 500));
+form.addEventListener('submit', onSubmit);
+const userInput = {};
+
+function onSaveInput(evt) {
+  userInput.email = evt.currentTarget.email.value;
+  userInput.message = evt.currentTarget.message.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userInput));
+}
+
+function checkStorageStatus() {
+  const savedData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  if (savedData) {
+    form.email.value = savedData.email;
+    form.message.value = savedData.message;
+  }
+}
+checkStorageStatus();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+
+  console.log({
+    email: userInput.email,
+    message: userInput.message,
+  });
+}
