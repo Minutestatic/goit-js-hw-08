d9e6ea6f8bd26c12ab00a998aed2584687ed7198
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-04-15 03:37:31 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const PLAYBACK_TIME_KEY = 'videoplayer-current-time';
+
+function updatePlaybackTime(data) {
+  localStorage.setItem(PLAYBACK_TIME_KEY, JSON.stringify(data.seconds));
+}
+
+player.on('timeupdate', throttle(updatePlaybackTime, 1000));
+
+const currentTime = JSON.parse(localStorage.getItem(PLAYBACK_TIME_KEY));
+
+player.setCurrentTime(0 || currentTime);
